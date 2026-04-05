e0203d427788438961cee7ae85558bee6657146c
Kasper Storgaard <kasper.storgaard@gmail.com>
2026-04-05 18:01:02 +0200

    add partial wrapper

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,4 +1,5 @@
 import { clsx } from "clsx/lite";
+import { Partial } from "fresh/runtime";
 
 import { define } from "#/core.ts";
 import { CookieBanner } from "#/islands/cookie-banner.tsx";
@@ -74,6 +75,8 @@ export default define.page(
         </head>
 
         <body
+          f-client-nav
+          f-view-transition
           className={clsx(
             "grow grid grid-cols-[minmax(var(--size-fluid-3),auto)_1fr_minmax(var(--size-fluid-3),auto)]",
             "sm:grid-cols-[minmax(var(--size-fluid-3),auto)_max-content_minmax(var(--size-fluid-3),auto)]",
@@ -83,7 +86,9 @@ export default define.page(
             "print:grid-cols-[1fr_max-content_1fr] print:grid-rows-[auto_1fr] print:gap-0",
           )}
         >
-          <Component />
+          <Partial name="body">
+            <Component />
+          </Partial>
           {!isTutorial && (
             <CookieBanner
               open={!state.cookieChoice}
