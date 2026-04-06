f3da8ead035f7596b59157c0269bc70482fedd4c
Kasper Storgaard <kasper.storgaard@gmail.com>
2026-04-06 16:03:26 +0200

    solution dialog tweaks

--- islands/solution-dialog.tsx
+++ islands/solution-dialog.tsx
@@ -52,8 +52,8 @@ export function SolutionDialog(
 
         <p>
           {savedName
-            ? "Claim your solve to see how others did it."
-            : "Pick a name and see how others did it."}
+            ? "Get your solve on the board."
+            : "Pick a name and get your solve on the board."}
         </p>
       </div>
 
@@ -69,7 +69,7 @@ export function SolutionDialog(
 
           <input
             name="name"
-            autocomplete="name"
+            autocomplete="username"
             placeholder="fx. Jungleboi87"
             value={savedName ?? undefined}
             required
@@ -95,19 +95,8 @@ export function SolutionDialog(
           <a
             href={getResetHref(href.value)}
           >
-            Try again
+            Start over
           </a>
-
-          <form method="dialog" className="inline">
-            <button
-              type="submit"
-              className="link p-0 bg-transparent"
-              formNoValidate
-              disabled={!hasSolution}
-            >
-              Close
-            </button>
-          </form>
         </div>
 
         <button
@@ -118,7 +107,7 @@ export function SolutionDialog(
         >
           {isSubmitting
             ? <Icon icon={Spinner} className="animate-spin" />
-            : "Claim your solve"}
+            : "Save"}
         </button>
       </div>
     </Dialog>
